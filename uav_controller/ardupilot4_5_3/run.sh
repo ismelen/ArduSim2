2b4d07b7d6f5fdeeaaaee70986001b47ab2ea900
Jamie-Wubben <wubben.jamie@outlook.com>
2024-07-12 11:10:33 +0200

    added dockerfiles, and updated documentation

---
+++ uav_controller/ardupilot4_5_3/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Run arducopter in the background
+./arducopter4_5_3 -S --base-port 5760 --home 39.482594,-0.346265,0.0,0.0 --model + --defaults copter.parm --sim-port-in 5501 --sim-port-out 5502 --rc-in-port 5503 --irlock-port 9005 &
+
+sleep 5 #wit for arducopter to boot
+# Run the Java application
+java -jar ./uavController.jar config.json &
+
+# Wait for all background processes to finish
+wait
