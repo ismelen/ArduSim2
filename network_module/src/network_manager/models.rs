a40f6e60d282428c62b1c68d57f429b44674af8f
ismelen <ismael.ma34k@gmail.com>
2026-02-18 15:59:17 +0100

    fix: time fields type change

--- network_module/src/network_manager/models.rs
+++ network_module/src/network_manager/models.rs
@@ -1,25 +1,31 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Debug, Clone)]
+#[derive(Deserialize, Debug, Clone, Serialize)]
 pub struct Message {
   pub sender_id: String,
   pub receiver_id: Option<String>,
   pub content: Option<String>,
   pub coords: Option<Coords>,
-  pub start: u64,
-  pub end: Option<u64>,
+  pub start: u128,
+  pub end: Option<u128>,
 }
 
-#[derive(Debug, Clone, Deserialize)]
+#[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Coords {
   pub x: f64,
   pub y: f64,
   pub z: f64,
 }
 
+impl PartialEq for Coords {
+  fn eq(&self, other: &Self) -> bool {
+    self.x == other.x && self.y == other.y && self.z == other.z
+  }
+}
+
 #[derive(Debug, Clone)]
 pub struct BusyState {
-  pub from: u64,
-  pub to: u64,
+  pub from: u128,
+  pub to: u128,
   pub overlapped: bool,
 }
